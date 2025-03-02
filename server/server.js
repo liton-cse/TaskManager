1dce443d52af6dbf51e841c2b666b2e57654d262
liton13 <litonakash13@gmail.com>
2025-03-02 22:15:22 +0600

    Again update the server file

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
-app.use(cors({ origin: "https://fastidious-moxie-d83ba5.netlify.app", credentials: true }));
+app.use(cors({ origin: "https://astonishing-jalebi-f735a4.netlify.app", credentials: true }));
 
 
 app.use('/api/auth', authRoutes);
